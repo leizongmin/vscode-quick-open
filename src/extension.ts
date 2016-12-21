be77e0efa0142258886525d53bfb8acb4eff4cc4
Zongmin Lei <leizongmin@gmail.com>
2016-12-21 12:13:39 +0800

    feat: open URL

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,7 @@ import * as fs from "fs";
 import * as path from "path";
 import * as os from "os";
 import * as vscode from "vscode";
+import * as leiDownload from "lei-download";
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -17,6 +18,33 @@ export function activate(context: vscode.ExtensionContext) {
 
   function getRootPath(): string {
     return vscode.workspace.rootPath || "/";
+  }
+
+  const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+  context.subscriptions.push(statusBar);
+  let statusBarTid: NodeJS.Timer;
+
+  function showStatusInfo(msg) {
+    statusBar.text = msg;
+    statusBar.color = "white";
+    statusBar.show();
+    autoHideStatusBar(2);
+  }
+
+  function showStatusWran(msg) {
+    statusBar.text = msg;
+    statusBar.color = "yellow";
+    statusBar.show();
+    autoHideStatusBar(10);
+  }
+
+  function autoHideStatusBar(s: number) {
+    if (statusBarTid) {
+      clearTimeout(statusBarTid);
+    }
+    statusBarTid = setTimeout(() => {
+      statusBar.hide();
+    }, s * 1000);
   }
 
   async function listDir(dir: string): Promise<vscode.QuickPickItem[]> {
@@ -106,6 +134,21 @@ export function activate(context: vscode.ExtensionContext) {
         prompt: "Enter the file path to open",
       });
     }
+    if (isURL(inputPath)) {
+      showStatusInfo(`Downloading ${ inputPath }`);
+      try {
+        inputPath = await download(inputPath, (size, total) => {
+          console.log("download progress: ", size, total);
+          if (total) {
+            showStatusInfo(`Downloading ${ (size / total * 100).toFixed(1) }%`);
+          } else {
+            showStatusInfo(`Downloading ${ (size / 1024).toFixed(0) }KB`);
+          }
+        });
+      } catch (err) {
+        showStatusWran(err.message);
+      }
+    }
     vscode.commands.executeCommand(CMD_QUICKOPEN, inputPath);
   }));
 }
@@ -138,4 +181,50 @@ function readdir(dir: string): Promise<string[]> {
       resolve(list);
     });
   });
+}
+
+/**
+ * download remote file and returns local temp path
+ */
+function download(url: string, onProgress: (size: number, total: number) => void): Promise<string> {
+  return new Promise((resolve, reject) => {
+    leiDownload(url, getTempNameFromURL(url), onProgress, (err, fileName) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(fileName);
+      }
+    });
+  });
+}
+
+/**
+ * returns temp file name from input URL
+ */
+function getTempNameFromURL(url: string): string {
+  const ext = path.extname(url);
+  const fileName = path.resolve(os.tmpdir(), randomString(20) + ext);
+  return fileName;
+}
+
+/**
+ * returns random string
+ */
+function randomString(size: number): string {
+  size = size || 6;
+  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const max = chars.length + 1;
+  let str = "";
+  while (size > 0) {
+    str += chars.charAt(Math.floor(Math.random() * max));
+    size -= 1;
+  }
+  return str;
+}
+
+/**
+ * returns true if the input string is an URL
+ */
+function isURL(url: string): boolean {
+  return url.slice(0, 7) === "http://" || url.slice(0, 8) === "https://";
 }
